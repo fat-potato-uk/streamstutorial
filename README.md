fb5a437541e23983856f5487ea6fee3c7daa506a
andrew <andrew@fat-potato.co.uk>
2019-08-28 17:23:07 +0100

    Added comment and hints

--- README.md
+++ README.md
@@ -5,3 +5,21 @@ fail. You must update the `StreamsChallenge` class so all the unit tests pass! T
 `StreamsChallengeAnswers`, but try and attempt the exercise without peeking!
 
 That's all!
+
+##### Hints
+
+* Streams are most analogous to Unix pipes, e.g. `find . -name "*.txt" | xargs grep -i sometext`
+whereby one operation feeds (streams) into the next, therefore, _collection.stream()_ will 
+iterate over the collection and return a _stream_ of values that can then be processed.
+
+* _collection.stream().action(item -> someFunction(item)) is the typical pattern found
+for streams behaviour, where action takes some sort of lambda function. Examples of this include: 
+    * _collection.stream().filter(x -> x.startsWith(...)_ will filter on the lambda function boolean outcome
+    * _collection.stream().sort()_ will sort the collection
+    * _collection.stream().action(...).collect(toList())_ will collate the output to a list
+    (other collection types may even exist...)
+    * _collection.stream().map(x -> return x)_ will perform an action then return the result
+    * _collection.stream().reduce((x1, x2) -> x1+x2)_ will reduce the contents of the collection
+    based on the function provided (this example would sum all the values if x was an int)
+
+* Google is your friend when it comes to Streams!
--- src/main/java/StreamsChallenge.java
+++ src/main/java/StreamsChallenge.java
@@ -7,50 +7,51 @@ class StreamsChallenge extends StreamsTutorial {
 
     @Override
     List<String> filterByStartingWithA() {
-        return null;
+        return null; // return this.getStringList().stream()...
     }
 
     @Override
     List<String> filterByStartingWithASorted() {
-        return null;
+        return null; // return this.getStringList().stream()...
     }
 
     @Override
     List<String> changeToUpperCaseAndReverseOrder() {
-        return null;
+        return null; // return this.getStringList().stream()...
     }
 
     @Override
     String concatenateToSingleHashDelimitedString() {
-        return null;
+        return null; // return this.getStringList().stream()...
     }
 
     @Override
     Map<String, Integer> mapOfNamesToSalary() {
-        return null;
+        return null; // return this.getPersonList().stream()...
     }
 
     @Override
     int totalSalaryCost() {
-        return 0;
+        return 0; // return this.getPersonList().stream()...
     }
 
     @Override
     long peopleWithThreeLetterNames() {
-        return 0;
+        return 0; // return this.getPersonList().stream()...
     }
 
     @Override
     void giveEveryoneAThousandPoundPayRise() {
+        // this.getPersonList().stream()...
     }
 
     @Override
     List<String> returnListOfPeopleFromPartialPersonListWithUnknownReplacedByDave() {
-        return List.of();
+        return List.of(); // return this.getPersonList().stream()...
     }
 
     @Override
     List<String> splitStringToCollection(String inputString, String separator) {
-        return List.of();
+        return List.of(); // return Pattern.compile(separator)
     }
 }
